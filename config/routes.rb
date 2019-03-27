943d29fc0c44d3221662d034ebee38daa2608453
makino.makino <doremisora3@gmail.com>
2019-03-27 16:07:32 +0000

    follow,unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,15 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  get 'signup', to: 'user#new'
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
 end
